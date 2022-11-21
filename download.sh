108efa1b66132db0abc1ffdbacd7819684393f11
Cedric Ziel <cedric.ziel@grafana.com>
2022-11-21 11:44:10 +0100

    Make download more flexible

--- download.sh
+++ download.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+export OTEL_VERSION=${OTEL_VERSION:-"0.64.1"}
+export OS=${OS:-"linux_amd64"}
+
 mkdir -p .download
 
 pushd .download
 echo "   -> Downloading OpenTelemetry collector"
 rm otelcol-contrib.tgz;
-wget -O otelcol-contrib.tgz https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.64.1/otelcol-contrib_0.64.1_linux_amd64.tar.gz
+wget -O otelcol-contrib.tgz "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v${OTEL_VERSION}/otelcol-contrib_${OTEL_VERSION}_${OS}.tar.gz"
 
 echo "   -> Extracting OpenTelemetry collector"
 tar xzf otelcol-contrib.tgz
@@ -14,3 +17,5 @@ popd
 
 echo "   -> Moving OpenTelemetry collector"
 mv .download/otelcol-contrib .
+
+echo "   Done."
